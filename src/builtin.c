0fa389d664d57eb8974e5c1b5c9135bfecbfb249
nnittala02 <navya@ku.edu>
2024-10-19 16:28:32 -0500

    cd works

--- src/builtin.c
+++ src/builtin.c
@@ -8,12 +8,14 @@
 
 #define BUFFER_SIZE 1024
 
-void run_echo(char *input) {
+void run_echo(char *input)
+{
     // Remove "echo " part and get the rest
     char *message = input + 5; // Skip the "echo " part
 
     // Remove leading spaces
-    while (*message == ' ') {
+    while (*message == ' ')
+    {
         message++;
     }
 
@@ -21,33 +23,38 @@ void run_echo(char *input) {
     int quote_state = 0;
 
     // Create a buffer to store the final output
-    char output[BUFFER_SIZE];  // Use a defined buffer size
+    char output[BUFFER_SIZE]; // Use a defined buffer size
     int output_index = 0;
 
     // Iterate through each character in the message
-    for (size_t i = 0; i < strlen(message); i++) {
+    for (size_t i = 0; i < strlen(message); i++)
+    {
         char current_char = message[i];
 
         // Toggle quote state for single quotes
-        if (current_char == '\'') {
+        if (current_char == '\'')
+        {
             quote_state++;
             continue; // Skip the quote character
         }
 
         // Toggle quote state for double quotes
-        if (current_char == '\"') {
+        if (current_char == '\"')
+        {
             quote_state++;
             continue; // Skip the quote character
         }
 
         // Handle environment variables
-        if (current_char == '$' && (quote_state % 2 == 0)) {
+        if (current_char == '$' && (quote_state % 2 == 0))
+        {
             // Get the name of the environment variable
             size_t var_start = i + 1;
             size_t var_length = 0;
 
             // Determine the length of the variable name (stop at non-alphanumeric or underscore)
-            while (isalnum(message[var_start + var_length]) || message[var_start + var_length] == '_') {
+            while (isalnum(message[var_start + var_length]) || message[var_start + var_length] == '_')
+            {
                 var_length++;
             }
 
@@ -58,27 +65,33 @@ void run_echo(char *input) {
 
             // Get the value of the environment variable
             char *var_value = getenv(var_name);
-            if (var_value != NULL) {
+            if (var_value != NULL)
+            {
                 // Add the environment variable value to output
                 size_t value_length = strlen(var_value);
-                if (output_index + value_length < BUFFER_SIZE) {
+                if (output_index + value_length < BUFFER_SIZE)
+                {
                     strncpy(&output[output_index], var_value, value_length);
                     output_index += value_length;
                 }
-            } else {
+            }
+            else
+            {
                 // Print an error message if the variable is not found (for debugging)
                 fprintf(stderr, "Warning: Environment variable %s not found.\n", var_name);
             }
 
             // Move the index past the variable name
             i += var_length; // Skip the variable name
-            continue; // Skip the `$` character
+            continue;        // Skip the `$` character
         }
 
         // If we encounter a space and we're not in quotes, treat it as a delimiter
-        if (current_char == ' ' && (quote_state % 2 == 0)) {
+        if (current_char == ' ' && (quote_state % 2 == 0))
+        {
             // Only add a space to output if the last character is not already a space
-            if (output_index > 0 && output[output_index - 1] != ' ') {
+            if (output_index > 0 && output[output_index - 1] != ' ')
+            {
                 output[output_index++] = ' ';
             }
             continue; // Skip adding the space character
@@ -150,23 +163,43 @@ void pwd()
     }
 }
 
-void cd(char *path) {
-    char cwd[BUFFER_SIZE];  // Buffer to hold the current working directory
+void cd(char *path)
+{
+    char cwd[BUFFER_SIZE];       // Buffer to hold the current working directory
     char *home = getenv("HOME"); // Get the HOME environment variable
 
-    // Check if no path is provided
-    if (path == NULL || strlen(path) == 0) {
-        // If no path, change to the HOME directory
-        path = home; 
+    // If no path is provided, change to the HOME directory
+    if (path == NULL || strlen(path) == 0)
+    {
+        path = home;
+    }
+
+    // Handle paths that start with "$"
+    if (path[0] == '$')
+    {
+        // Get the environment variable name (e.g., "HOME")
+        char *env_var_name = path + 1;              // Skip the '$'
+        char *env_var_value = getenv(env_var_name); // Get the environment variable value
+
+        if (env_var_value)
+        {
+            path = env_var_value; // Use the expanded environment variable value as the path
+        }
+        else
+        {
+            fprintf(stderr, "cd: No such environment variable: %s\n", env_var_name);
+            return;
+        }
     }
 
     // Change the current working directory
-    if (chdir(path) == -1) {
-        perror("cd error");  // Print error if chdir fails
+    if (chdir(path) == -1)
+    {
+        perror("cd error"); // Print error if chdir fails
         return;
     }
 
     // Update the PWD environment variable
-    getcwd(cwd, sizeof(cwd));  // Get the new current working directory
-    setenv("PWD", cwd, 1);  // Update PWD environment variable
+    getcwd(cwd, sizeof(cwd)); // Get the new current working directory
+    setenv("PWD", cwd, 1);    // Update PWD environment variable
 }
