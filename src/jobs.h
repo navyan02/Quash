608e5b353f2d8620ff841f70ef8e325bfe7db7c2
Shayaan <140364268+Shayaan04@users.noreply.github.com>
2024-10-21 17:18:15 -0500

    Update jobs.h

--- src/jobs.h
+++ src/jobs.h
@@ -1,19 +1,19 @@
 #ifndef JOBS_H
 #define JOBS_H
+
+#include <sys/types.h>
 #define BUFFER_SIZE 1024
 
-typedef struct Job
-{
-    int job_id;
-    pid_t pid;
-    char command[BUFFER_SIZE];
-    struct Job *next;
+typedef struct Job {
+    int job_id;            // Unique job ID
+    pid_t pid;            // Process ID of the job
+    char command[BUFFER_SIZE]; // Command associated with the job
+    struct Job *next;     // Pointer to the next job in the list
 } Job;
-
-extern int next_job_id; // Declare next_job_id here
 
 void add_job(pid_t pid, char *command);
 void remove_job(pid_t pid);
 void list_jobs();
+void free_jobs();
 
 #endif // JOBS_H
